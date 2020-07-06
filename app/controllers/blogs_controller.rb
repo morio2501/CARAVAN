8b4ab8b85bac0ad3ac1deaec99ce1d754c165d28
vagrant <vagrant@localhost.localdomain>
2020-07-06 12:20:50 +0000

    [Finish] 4-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -26,14 +26,14 @@ class BlogsController < ApplicationController
   	blog.update(blog_params)
   	redirect_to blog_path(blog)
   end
-  def destory
-  	blog = Blog.find(params[:id])
-  	blog.destory
-  	redirect_to blogs_path
+ def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
 
   private
   def blog_params
   	params.require(:blog).permit(:title, :category, :body)
   end
-
 end
